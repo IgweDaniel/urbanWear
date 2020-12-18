990fc4adf1198b1683c1bad7f8a997119e289a61
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 13:13:03 +0100

    updated settings

--- urbanWear/settings.py
+++ urbanWear/settings.py
@@ -40,12 +40,12 @@ ALLOWED_HOSTS = ['http://localhost:3000', '127.0.0.1',
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
