9c3f97cdd6a2753ec778ab40cce1a8cc4f5b7f2d
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 13:33:08 +0100

    checking folder stufff

--- urbanWear/wsgi.py
+++ urbanWear/wsgi.py
@@ -28,4 +28,6 @@ application = get_wsgi_application()
 
 application = WhiteNoise(
     application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
+application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
 # application = WhiteNoise(application, root='/static/staticfiles')
