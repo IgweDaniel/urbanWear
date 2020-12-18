41f3e98b7012d9230ca66825bfaaf3f2f344eb13
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 12:52:03 +0100

    cheking now

--- urbanWear/settings.py
+++ urbanWear/settings.py
@@ -30,6 +30,7 @@ load_dotenv(dotenv_path=env_path)
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 DEBUG = True
 
 # ALLOWED_HOSTS = []
