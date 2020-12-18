897c1d06b54214c2215d23668c7036aa4f736f71
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 11:41:14 +0100

    removed preconfigured settings acess

--- urbanWear/wsgi.py
+++ urbanWear/wsgi.py
@@ -8,19 +8,21 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
 from whitenoise import WhiteNoise
-from django.conf import settings
+# from django.conf import settings
 # from my_project import MyWSGIApp
 
 # application = MyWSGIApp()
 # application = WhiteNoise(application, root='/path/to/static/files')
 # application.add_files('/path/to/more/static/files', prefix='more-files/')
 
-BASE_DIR = settings.BASE_DIR
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urbanWear.settings')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 
