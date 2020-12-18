14512add9dfd1eed89b38f1feb9084d70858cc4d
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 13:02:18 +0100

    debug turned off

--- urbanWear/settings.py
+++ urbanWear/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['http://localhost:3000', '127.0.0.1',
