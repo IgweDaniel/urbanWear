9591781662e04d94dc9eec0803c5abed504b7bf4
IgweDaniel <danielchibuzoigwe@gmail.com>
2020-12-18 16:17:19 +0100

    now trying media again

--- urbanWear/wsgi.py
+++ urbanWear/wsgi.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 
-# application = WhiteNoise(
-#     application, root=os.path.join(BASE_DIR, 'staticfiles'))
+application = WhiteNoise(
+    application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
-# application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
+application.add_files(os.path.join(BASE_DIR, 'media'), prefix='/media/')
